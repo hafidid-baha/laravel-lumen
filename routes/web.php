14d213aeec433a650baecc9bd008a1612497db61
Evel25 <bi3.h300@gmail.com>
2020-06-15 16:13:54 +0100

    adding the app routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+// $router->get('/', function () use ($router) {
+//     return $router->app->version();
+// });
+
+$router->group(['prefix'=>'api/v1'], function() use($router){
+
+    $router->get('/items', 'ProductController@index');
+    $router->post('/items', 'ProductController@create');
+    $router->get('/items/{id}', 'ProductController@show');
+    $router->put('/items/{id}', 'ProductController@update');
+    $router->delete('/items/{id}', 'ProductController@destroy');
+
 });
